e27c435c40c9711b60ad8e9128fe4a482c65708b
0xack13 <0xack13@gmail.com>
2015-08-02 11:45:17 +0300

    Multiple controllers

--- prodList.js
+++ prodList.js
@@ -1,6 +1,7 @@
 var app = angular.module("sampleApp", ["firebase"]);
 
-app.controller("ListCtrl", function ($scope, $firebaseArray) {
+//Creating multpile controllers for each function
+var ListCtrl = app.controller("ListCtrl", function ($scope, $firebaseArray) {
     var productsList;
     var ref = new Firebase("https://inven.firebaseio.com");
 
@@ -12,12 +13,31 @@ app.controller("ListCtrl", function ($scope, $firebaseArray) {
         productsList = $scope.productsList;
 
         angular.forEach(productsList, function (value, key) {
-
             console.log("The product details of " + key 
                         + " is : Description = " + value.Description 
                         + ", Stock = " + value.Stock             
                        );
-            
+        });
+    });
+});
+
+
+var addCtrl = app.controller("addCtrl", function ($scope, $firebaseArray) {
+    var productsList;
+    var ref = new Firebase("https://inven.firebaseio.com");
+
+    $scope.messages = $firebaseArray(ref);
+
+    ref.child("Product").on("value", function (snapshot) {
+
+        $scope.productsList = snapshot.val();
+        productsList = $scope.productsList;
+
+        angular.forEach(productsList, function (value, key) {
+            console.log("The product details of " + key 
+                        + " is : Description = " + value.Description 
+                        + ", Stock = " + value.Stock             
+                       );
         });
     });
 });
